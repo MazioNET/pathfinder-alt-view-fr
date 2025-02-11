c5dd663a9df0b05bca50fe574220b294b3b521e9
MazioNET <play@mazio.net>
2025-02-11 16:06:04 +0100

    First commit
    Initialisation du projet

---
+++ src/app/siteStructure/side-view/side-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-view.component.html',
+  styleUrl: './side-view.component.less'
+})
+export class SideViewComponent {
+
+}
